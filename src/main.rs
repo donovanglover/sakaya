b41e968e286380d301dca0471c4c72a5d6f159b5
Donovan Glover <donovan@dglover.co>
2023-07-19 22:33:33 -0400

    feat: Add all commands

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,10 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Create a new nixos-container.
+    /// List all containers.
+    List {},
+
+    /// Create a new container.
     Create {
         #[arg(long)]
         nixos_path: PathBuf,
@@ -48,18 +51,76 @@ enum Commands {
 
         #[arg(long)]
         local_address: IpAddr,
-    }
+    },
+
+    /// Destroy an existing container.
+    Destroy {},
+
+    /// Start an existing container.
+    Start {},
+
+    /// Stop an existing container.
+    Stop {},
+
+    /// Terminate a container.
+    Terminate {},
+
+    /// Get the status of a container.
+    Status {},
+
+    /// Update a container.
+    Update {},
+
+    /// Login to a container.
+    Login {},
+
+    /// Login as root to a container.
+    RootLogin {},
+
+    /// Run a command from a container.
+    Run {},
+
+    /// Show the IP address of a container.
+    ShowIp {},
+
+    /// Show the host key of a container.
+    ShowHostKey {},
 }
 
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
+        Some(Commands::List { .. }) => {}
+
         Some(Commands::Create { auto_start, .. }) => {
             if *auto_start {
                 println!("auto_start");
             }
         }
+
+        Some(Commands::Destroy { .. }) => {}
+
+        Some(Commands::Start { .. }) => {}
+
+        Some(Commands::Stop { .. }) => {}
+
+        Some(Commands::Terminate { .. }) => {}
+
+        Some(Commands::Status { .. }) => {}
+
+        Some(Commands::Update { .. }) => {}
+
+        Some(Commands::Login { .. }) => {}
+
+        Some(Commands::RootLogin { .. }) => {}
+
+        Some(Commands::Run { .. }) => {}
+
+        Some(Commands::ShowIp { .. }) => {}
+
+        Some(Commands::ShowHostKey { .. }) => {}
+
         None => {}
     }
 }
