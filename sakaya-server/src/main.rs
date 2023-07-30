40f4258de60adac2761d058350ce0d194293794f
Donovan Glover <donovan@dglover.co>
2023-07-30 15:11:01 -0400

    server: Remove systemd-nspawn check
    This doesn't actually work and I'm not interested in making it work
    since once I set this up, the server should never be started on the
    host anyway.

--- sakaya-server/src/main.rs
+++ sakaya-server/src/main.rs
@@ -39,16 +39,7 @@ fn post(data: Json<MyCommand<'_>>) -> String {
 
 #[launch]
 fn rocket() -> _ {
-    let virt = Command::new("systemd-detect-virt")
-        .output()
-        .expect("Failed to detect");
-
-    let mut host_ip_from_container = IpAddr::V4(Ipv4Addr::new(192, 168, 100, 49));
-
-    if String::from_utf8_lossy(&virt.stdout) != "systemd-nspawn" {
-        println!("WARNING: sakaya-server was NOT executed inside of a systemd-nspawn container.");
-        host_ip_from_container = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-    }
+    let host_ip_from_container = IpAddr::V4(Ipv4Addr::new(192, 168, 100, 49));
 
     let figment = rocket::Config::figment()
         .merge(("port", 39493))
