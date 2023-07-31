120973c340a345be0d491ee676d8a088b4def6c4
Donovan Glover <donovan@dglover.co>
2023-07-31 19:47:59 -0400

    cli: Surround desktop entry path in quotes
    This fixes an issue where paths with spaces would break the resulting
    desktop file.

--- sakaya-cli/src/main.rs
+++ sakaya-cli/src/main.rs
@@ -27,7 +27,7 @@ fn make_desktop_file(output_location: &str, file_name: &str, full_path: &str) {
     let mut output: String = "[Desktop Entry]".to_owned() + "\n";
     output.push_str("Type=Application\n");
     output.push_str(&("Name=".to_owned() + file_name + "\n"));
-    output.push_str(&("Exec=sakaya ".to_owned() + full_path + "\n"));
+    output.push_str(&("Exec=sakaya \"".to_owned() + full_path + "\"\n"));
 
     let _ = fs::write(output_location, output);
 }
