f216aceb070076ecb2f511484ceb087600dbba20
Donovan Glover <donovan@dglover.co>
2023-07-31 12:23:57 -0400

    cli: Add working implementation
    This *works*, which is pretty great.

--- sakaya-cli/src/main.rs
+++ sakaya-cli/src/main.rs
@@ -1,3 +1,43 @@
+use clap::Parser;
+use std::collections::HashMap;
+use std::path::Path;
+use reqwest::blocking::ClientBuilder;
+
+#[derive(Parser)]
+#[command(version)]
+#[command(arg_required_else_help = true)]
+struct Cli {
+    /// Path to the executable to run.
+    executable: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let path = Path::new(&cli.executable);
+
+    if path.exists() {
+        let full_path = path.canonicalize().unwrap();
+        let full_path_str = full_path.to_str().expect("Couldn't convert to str");
+
+        // TODO: Don't hardcode this?
+        if full_path_str.contains("/home/user/containers/wine") {
+            let container_path = full_path_str.replace("/home/user/containers/wine", "/mnt");
+            let path_str = path.to_str().expect("Couldn't convert to str");
+
+            println!("Running {} as {}...", path_str, container_path);
+
+            let mut map = HashMap::new();
+            map.insert("wine", "");
+            map.insert("path", &container_path);
+
+            let client = ClientBuilder::new().timeout(None).build().unwrap();
+            let result = client.post("http://192.168.100.49:39493").json(&map).send().expect("Couldn't request sakaya-server").text();
+
+            println!("{:?}", result)
+        } else {
+            // TODO: Get rid of else statements
+            println!("File is NOT in path")
+        }
+    }
 }
