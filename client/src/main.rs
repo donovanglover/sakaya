8ca428edd4b3b4abf707c17eaa727a926ac2601a
Donovan Glover <donovan@dglover.co>
2023-07-31 09:52:26 -0400

    client: Only ask for container name
    This simplifies things a bit and makes the client only have one job:
    Start sakaya-server in the nixos-container from the host.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,37 +1,26 @@
-use clap::{Parser, Subcommand};
+use clap::Parser;
 use std::process::Command;
 
 #[derive(Parser)]
 #[command(version)]
+#[command(arg_required_else_help = true)]
 struct Cli {
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Start sakaya in a new container or reboot an existing one.
-    New {
-        container_name: String
-    },
+    container: String,
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    match &cli.command {
-        Some(Commands::New { container_name }) => {
-            let status = Command::new("nixos-container").args(["status", container_name]).output().expect("Failed to get nixos-container status");
+    let status = Command::new("nixos-container").args(["status", &cli.container]).output().expect("Failed to get nixos-container status");
 
-            if String::from_utf8_lossy(&status.stdout) == "down\n" {
-                let _ = Command::new("nixos-container").args(["start", container_name]).output().expect("Failed to run nixos-container start");
-            } else {
-                let _ = Command::new("machinectl").args(["reboot", container_name]).output().expect("Failed to run machinectl reboot");
-            }
-
-            let _ = Command::new("machinectl").args(["shell", "user@wine", "/usr/bin/env", "sakaya-server"]).output().expect("Failed to start a user shell with sakaya-server");
-        }
-
-        None => {}
+    if String::from_utf8_lossy(&status.stdout) == "down\n" {
+        let _ = Command::new("nixos-container").args(["start", &cli.container]).output().expect("Failed to run nixos-container start");
+    } else {
+        let _ = Command::new("machinectl").args(["reboot", &cli.container]).output().expect("Failed to run machinectl reboot");
     }
+
+    let mut user: String = "user@".to_owned();
+    user.push_str(&cli.container);
+
+    let _ = Command::new("machinectl").args(["shell", user.as_str(), "/usr/bin/env", "sakaya-server"]).output().expect("Failed to start a user shell with sakaya-server");
 }
