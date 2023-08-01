df92bc152a71fe736c9bd9dff5c8088ec93685a6
Donovan Glover <donovan@dglover.co>
2023-08-01 10:05:07 -0400

    gui: Simplify libadwaita usage

--- sakaya-gui/src/main.rs
+++ sakaya-gui/src/main.rs
@@ -1,15 +1,73 @@
-// This example is copied from https://gitlab.gnome.org/World/Rust/libadwaita-rs/-/blob/master/libadwaita/examples/hello-world.rs
-// The only part that is specific to the usage of gtk4-layer-shell is encased by comments
-
 use libadwaita as adw;
 
 use adw::prelude::*;
 use adw::{ActionRow, ApplicationWindow};
-use gtk::{Application, Box, ListBox, Orientation};
+use gtk::{Application, Box, Orientation};
+
+fn activate(application: &gtk::Application) {
+    // ActionRows are only available in Adwaita
+    let row = ActionRow::builder()
+        .activatable(true)
+        .selectable(false)
+        .title("Click me")
+        .build();
+    row.connect_activated(|_| {
+        eprintln!("Clicked!");
+    });
+    // Combine the content in a box
+    let content = Box::new(Orientation::Vertical, 0);
+    content.append(&row);
+
+    let window = ApplicationWindow::builder()
+        .application(application)
+        .default_width(350)
+        // add content to window
+        .content(&content)
+        .build();
+
+    window.set_visible(true);
+
+    // Create a normal GTK window however you like
+    let window = gtk::ApplicationWindow::new(application);
+
+    // Before the window is first realized, set it up to be a layer surface
+    gtk_layer_shell::init_for_window(&window);
+
+    // Display it above normal windows
+    gtk_layer_shell::set_layer(&window, gtk_layer_shell::Layer::Overlay);
+
+    // Push other windows out of the way
+    gtk_layer_shell::auto_exclusive_zone_enable(&window);
+
+    // The margins are the gaps around the window's edges
+    // Margins and anchors can be set like this...
+    gtk_layer_shell::set_margin(&window, gtk_layer_shell::Edge::Left, 40);
+    gtk_layer_shell::set_margin(&window, gtk_layer_shell::Edge::Right, 40);
+    gtk_layer_shell::set_margin(&window, gtk_layer_shell::Edge::Top, 20);
+
+    // ... or like this
+    // Anchors are if the window is pinned to each edge of the output
+    let anchors = [
+        (gtk_layer_shell::Edge::Left, true),
+        (gtk_layer_shell::Edge::Right, true),
+        (gtk_layer_shell::Edge::Top, false),
+        (gtk_layer_shell::Edge::Bottom, true),
+    ];
+
+    for (anchor, state) in anchors {
+        gtk_layer_shell::set_anchor(&window, anchor, state);
+    }
+
+    // Set up a widget
+    let label = gtk::Label::new(Some(""));
+    label.set_markup("<span font_desc=\"20.0\">GTK Layer Shell example!</span>");
+    window.set_child(Some(&label));
+    window.set_visible(true)
+}
 
 fn main() {
     let application = Application::builder()
-        .application_id("com.example.FirstAdwaitaApp")
+        .application_id("sakaya")
         .build();
 
     application.connect_startup(|_| {
@@ -17,64 +75,7 @@ fn main() {
     });
 
     application.connect_activate(|app| {
-        // ActionRows are only available in Adwaita
-        let row = ActionRow::builder()
-            .activatable(true)
-            .selectable(false)
-            .title("Click me")
-            .build();
-        row.connect_activated(|_| {
-            eprintln!("Clicked!");
-        });
-
-        let list = ListBox::builder()
-            .margin_top(32)
-            .margin_end(32)
-            .margin_bottom(32)
-            .margin_start(32)
-            // the content class makes the list look nicer
-            .css_classes(vec![String::from("content")])
-            .build();
-        list.append(&row);
-
-        // Combine the content in a box
-        let content = Box::new(Orientation::Vertical, 0);
-        content.append(&list);
-
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .default_width(350)
-            // add content to window
-            .content(&content)
-            .build();
-
-        // #################################
-        // Part that is specific to use gtk4-layer-shell begins
-
-        // Before the window is first realized, set it up to be a layer surface
-        gtk_layer_shell::init_for_window(&window);
-
-        // Display above normal windows
-        gtk_layer_shell::set_layer(&window, gtk_layer_shell::Layer::Overlay);
-
-        // Push other windows out of the way
-        gtk_layer_shell::auto_exclusive_zone_enable(&window);
-
-        // Anchors are if the window is pinned to each edge of the output
-        let anchors = [
-            (gtk_layer_shell::Edge::Left, true),
-            (gtk_layer_shell::Edge::Right, true),
-            (gtk_layer_shell::Edge::Top, false),
-            (gtk_layer_shell::Edge::Bottom, true),
-        ];
-
-        for (anchor, state) in anchors {
-            gtk_layer_shell::set_anchor(&window, anchor, state);
-        }
-        // Part that is specific to use gtk4-layer-shell ends
-        // #################################
-
-        window.set_visible(true);
+        activate(app);
     });
 
     application.run();
