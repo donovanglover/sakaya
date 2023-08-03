33f5b9fa52b0094989d39d0194829bbb65666a43
Donovan Glover <donovan@dglover.co>
2023-08-03 11:00:43 -0400

    meta: Add shell completions
    Generated from build.rs.

---
+++ completions/sakaya.bash
@@ -0,0 +1,38 @@
+_sakaya() {
+    local i cur prev opts cmd
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    cmd=""
+    opts=""
+
+    for i in ${COMP_WORDS[@]}
+    do
+        case "${cmd},${i}" in
+            ",$1")
+                cmd="sakaya"
+                ;;
+            *)
+                ;;
+        esac
+    done
+
+    case "${cmd}" in
+        sakaya)
+            opts="-h -V --help --version [EXECUTABLE]"
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+    esac
+}
+
+complete -F _sakaya -o bashdefault -o default sakaya
---
+++ completions/sakaya.fish
@@ -0,0 +1,2 @@
+complete -c sakaya -s h -l help -d 'Print help'
+complete -c sakaya -s V -l version -d 'Print version'
