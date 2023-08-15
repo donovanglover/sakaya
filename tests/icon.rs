a80db67855ab73d4d795a90a91c4c7cb24b9c129
Donovan Glover <donovan@dglover.co>
2023-08-15 17:34:52 -0400

    chore: Update icon tests

--- tests/icon.rs
+++ tests/icon.rs
@@ -1,7 +1,15 @@
 use sakaya::get_first_ico_file;
+use sakaya::convert_largest_square_image_in_ico_to_png;
 
 #[test]
-fn gets_icon_files_from_osu_exe() {
+fn gets_ico_from_osu_exe() {
     // https://m1.ppy.sh/r/osu!install.exe
-    get_first_ico_file("osu!install.exe");
+    assert!(get_first_ico_file("osu!install.exe").is_some(), "osu!install.exe returns an ico group");
+}
+
+#[test]
+fn gets_largest_osu_icon() {
+    if let Some(ico) = get_first_ico_file("osu!install.exe") {
+        convert_largest_square_image_in_ico_to_png(ico);
+    }
 }
