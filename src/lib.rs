a10e43f0977ae161c99bd0bba03c6b4e92cf0299
Donovan Glover <donovan@dglover.co>
2023-08-15 12:04:57 -0400

    feat: Clarify that we're getting the first .ico file

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@ pub fn is_container() -> bool {
     fs::read("/run/systemd/container").is_ok()
 }
 
-/// Given an .exe file, return the ico files inside it
-pub fn get_ico_files(input_bin: &str) {
+/// Given an .exe file, return the first .ico file inside it
+pub fn get_first_ico_file(input_bin: &str) {
     let map = FileMap::open(input_bin).expect("Error opening the binary");
     let file = PeFile::from_bytes(&map).expect("Error parsing the binary");
     let resources = file.resources().expect("Error binary does not have resources");
