f725c4d3bbe2a5a88a9c50e2d67464dd1efd3ce3
Donovan Glover <donovan@dglover.co>
2023-08-15 10:00:53 -0400

    feat: Add native .ico extract support
    Now the icoextract executable is no longer required.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
 use std::fs;
+use pelite::{FileMap, PeFile};
 
 /// Checks if we're inside a container
 pub fn is_container() -> bool {
     fs::read("/run/systemd/container").is_ok()
+}
+
+/// Given an .exe file, return the ico files inside it
+pub fn get_ico_files(input_bin: &str) {
+    let map = FileMap::open(input_bin).expect("Error opening the binary");
+    let file = PeFile::from_bytes(&map).expect("Error parsing the binary");
+    let resources = file.resources().expect("Error binary does not have resources");
+
+    for (name, group) in resources.icons().filter_map(Result::ok) {
+        let mut contents = Vec::new();
+
+        group.write(&mut contents).unwrap();
+        fs::write(format!("{name}.ico"), contents).unwrap();
+    }
 }
