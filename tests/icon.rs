d0d6caa5a4cf5b55abf26c5f667f2a71136994a2
Donovan Glover <donovan@dglover.co>
2023-08-18 22:43:25 -0400

    tests: Ensure that executables with varying icos work

--- tests/icon.rs
+++ tests/icon.rs
@@ -12,7 +12,14 @@ fn gets_ico_from_osu_exe() {
 
 #[test]
 fn gets_largest_osu_icon() {
-    if let Some(ico) = get_first_ico_file("osu!install.exe") {
-        assert!(convert_largest_square_image_in_ico_to_png(ico, "test.png").is_ok());
+    if let Some(ico) = get_first_ico_file("./in-out/osu!install.exe") {
+        assert!(convert_largest_square_image_in_ico_to_png(ico, "./in-out/osu!install.png").is_ok());
+    }
+}
+
+#[test]
+fn gets_the_first_icon_if_there_is_only_one() {
+    if let Some(ico) = get_first_ico_file("./in-out/test.exe") {
+        assert!(convert_largest_square_image_in_ico_to_png(ico, "./in-out/test.png").is_ok());
     }
 }
