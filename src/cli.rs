394ab3dc40e036cbba3dc72aaddda197bbbc43d0
Donovan Glover <donovan@dglover.co>
2023-10-27 22:20:55 -0400

    feat(cli): Add colored output to --help

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,18 @@
+use clap::builder::styling::{AnsiColor, Effects, Styles};
 use clap::Parser;
 use std::net::SocketAddrV4;
 use std::path::PathBuf;
 
+fn styles() -> Styles {
+    Styles::styled()
+        .header(AnsiColor::Red.on_default() | Effects::BOLD)
+        .usage(AnsiColor::Red.on_default() | Effects::BOLD)
+        .literal(AnsiColor::Blue.on_default() | Effects::BOLD)
+        .placeholder(AnsiColor::Green.on_default())
+}
+
 #[derive(Parser)]
-#[command(author, version, about)]
+#[command(author, version, about, styles = styles())]
 pub struct Cli {
     /// Path to the executable to run.
     pub file: Option<PathBuf>,
