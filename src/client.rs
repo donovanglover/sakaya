9ef063ea2924067206493c46da389ad90988ccd7
Donovan Glover <donovan@dglover.co>
2023-10-29 14:48:47 -0400

    chore: Run cargo fmt

--- src/client.rs
+++ src/client.rs
@@ -51,7 +51,10 @@ pub fn exec(address: SocketAddrV4, path: &Path, directory: &str) {
         if request(address, &container_path, wine_prefix).is_ok() {
             notify(&format!("Closed {file_name}."), Some(&icon));
         } else {
-            notify(&format!("Error: sakaya server is not accessible."), Some(&icon));
+            notify(
+                &format!("Error: sakaya server is not accessible."),
+                Some(&icon),
+            );
         }
     }
 }
