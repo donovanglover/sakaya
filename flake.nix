acfbad90f0604a29cda0ce04fcee5f2132bb2605
Donovan Glover <donovan@dglover.co>
2024-04-07 18:01:40 -0400

    nix: Fix flake package outputs

--- flake.nix
+++ flake.nix
@@ -3,14 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, utils }: utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system: {
-    packages = {
-      sakaya = import ./nix/package.nix;
-
-      default = self.packages.${system}.sakaya;
+  outputs = { nixpkgs, ... }: {
+    packages.x86_64-linux = {
+      sakaya = nixpkgs.callPackage ./nix/package.nix { };
+      default = nixpkgs.callPackage ./nix/package.nix { };
     };
-  });
+  };
 }
