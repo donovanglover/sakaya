1e38399fbbe6d58a570c856d4a1860136df6dca4
Donovan Glover <donovan@dglover.co>
2024-10-22 22:13:03 -0400

    refactor: split icon logic into separate module

---
+++ src/client/icon.rs
@@ -0,0 +1,59 @@
+use std::fs::File;
+use pelite::{FileMap, PeFile};
+use std::io::Cursor;
+
+/// Given an .exe file, return the first .ico file inside it
+pub fn get_first_ico_file(input_bin: &str) -> Option<Cursor<Vec<u8>>> {
+    let map = FileMap::open(input_bin).expect("Error opening the binary");
+    let file = PeFile::from_bytes(&map).expect("Error parsing the binary");
+    let resources = file
+        .resources()
+        .expect("Error binary does not have resources");
+
+    if let Some((_, group)) = resources.icons().find_map(Result::ok) {
+        let mut contents = Vec::new();
+        group.write(&mut contents).unwrap();
+
+        return Some(Cursor::new(contents));
+    }
+
+    None
+}
+
+/// Given an .ico with multiple images, return the largest one that's a square
+pub fn convert_largest_square_image_in_ico_to_png(
+    buf: Cursor<Vec<u8>>,
+    output_path: &str,
+) -> Result<(), std::io::Error> {
+    let icondir = ico::IconDir::read(buf)?;
+    let mut largest_size = 0;
+    let mut largest_index = 0;
+
+    for (i, image) in icondir.entries().iter().enumerate() {
+        let width = image.width();
+
+        if width == image.height() && width > largest_size {
+            largest_size = width;
+            largest_index = i;
+        }
+    }
+
+    let image = icondir.entries()[largest_index].decode()?;
+    let out_file = File::create(output_path).unwrap();
+
+    image.write_png(out_file)
+}
+
+/// Makes an icon for the application
+pub fn make_icon(input_bin: &str, file_name: &str) -> String {
+    let home = home::home_dir().unwrap();
+    let home = home.to_str().unwrap();
+
+    let output_path = &format!("{home}/.local/share/icons/{file_name}.png");
+
+    if let Some(icon) = get_first_ico_file(input_bin) {
+        let _ = convert_largest_square_image_in_ico_to_png(icon, output_path);
+    }
+
+    output_path.to_string()
+}
--- src/client/mod.rs
+++ src/client/mod.rs
@@ -3,9 +3,11 @@ use crate::util::notify;
 use clap::Parser;
 use pelite::{FileMap, PeFile};
 use std::fs;
-use std::io::Cursor;
 use std::net::SocketAddrV4;
 use std::path::Path;
+
+mod icon;
+pub use icon::*;
 
 /// https://github.com/MicrosoftDocs/win32/blob/docs/desktop-src/Debug/pe-format.md#machine-types
 const IMAGE_FILE_MACHINE_I386: u16 = 0x14C;
@@ -66,24 +68,6 @@ pub fn request(address: SocketAddrV4, path: &str, wine_prefix: &str) -> Result<(
     Ok(())
 }
 
-/// Given an .exe file, return the first .ico file inside it
-pub fn get_first_ico_file(input_bin: &str) -> Option<Cursor<Vec<u8>>> {
-    let map = FileMap::open(input_bin).expect("Error opening the binary");
-    let file = PeFile::from_bytes(&map).expect("Error parsing the binary");
-    let resources = file
-        .resources()
-        .expect("Error binary does not have resources");
-
-    if let Some((_, group)) = resources.icons().find_map(Result::ok) {
-        let mut contents = Vec::new();
-        group.write(&mut contents).unwrap();
-
-        return Some(Cursor::new(contents));
-    }
-
-    None
-}
-
 /// Gets whether the exe is 32 or 64-bit
 pub fn get_target_machine(input_bin: &str) -> u8 {
     let map = FileMap::open(input_bin).expect("Error opening the binary");
@@ -95,44 +79,6 @@ pub fn get_target_machine(input_bin: &str) -> u8 {
         IMAGE_FILE_MACHINE_AMD64 => 64,
         _ => 0,
     }
-}
-
-/// Given an .ico with multiple images, return the largest one that's a square
-pub fn convert_largest_square_image_in_ico_to_png(
-    buf: Cursor<Vec<u8>>,
-    output_path: &str,
-) -> Result<(), std::io::Error> {
-    let icondir = ico::IconDir::read(buf)?;
-    let mut largest_size = 0;
-    let mut largest_index = 0;
-
-    for (i, image) in icondir.entries().iter().enumerate() {
-        let width = image.width();
-
-        if width == image.height() && width > largest_size {
-            largest_size = width;
-            largest_index = i;
-        }
-    }
-
-    let image = icondir.entries()[largest_index].decode()?;
-    let out_file = fs::File::create(output_path).unwrap();
-
-    image.write_png(out_file)
-}
-
-/// Makes an icon for the application
-pub fn make_icon(input_bin: &str, file_name: &str) -> String {
-    let home = home::home_dir().unwrap();
-    let home = home.to_str().unwrap();
-
-    let output_path = &format!("{home}/.local/share/icons/{file_name}.png");
-
-    if let Some(icon) = get_first_ico_file(input_bin) {
-        let _ = convert_largest_square_image_in_ico_to_png(icon, output_path);
-    }
-
-    output_path.to_string()
 }
 
 /// Makes a desktop file for the application
