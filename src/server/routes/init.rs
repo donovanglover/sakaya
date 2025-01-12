40cb94ec54292f1ddcda517bd77e56707affa3e2
Donovan Glover <donovan@dglover.co>
2025-01-12 01:11:02 -0500

    server: notify when running winetricks
    Makes it easier to see progress and debug issues.

--- src/server/routes/init.rs
+++ src/server/routes/init.rs
@@ -1,56 +1,57 @@
 use axum::extract::Json;
-use std::process::Command;
+use std::{collections::HashMap, process::Command};
 
 use crate::{state::Options, util::notify};
 
 /// Create a wine prefix
 pub async fn init(Json(options): Json<Options>) -> Result<String, &'static str> {
+    let envs = Options::vars(&options);
+
     notify("Initializing wine prefix...", None);
 
     Command::new("wineboot")
-        .envs(Options::vars(&options))
+        .envs(&envs)
         .output()
         .unwrap();
 
-    Command::new("winetricks")
-        .arg("fontsmooth=rgb")
-        .envs(Options::vars(&options))
-        .output()
-        .unwrap();
+    let commands = [
+        "fontsmooth=rgb",
+        "dotnet35",
+        "dotnet40",
+        "vcrun2003",
+        "vcrun2005",
+        "vcrun2008",
+        "vcrun2010",
+        "vcrun2012",
+        "vcrun2013",
+        "vcrun2015",
+        "lavfilters",
+        "alldlls=default",
+        "quartz",
+        "dxvk",
+        "wmp10",
+        "renderer=gdi",
+    ];
 
-    Command::new("winetricks")
-        .arg("-q")
-        .args([
-            "dotnet35",
-            "dotnet40",
-            "vcrun2003",
-            "vcrun2005",
-            "vcrun2008",
-            "vcrun2010",
-            "vcrun2012",
-            "vcrun2013",
-            "vcrun2015",
-            "lavfilters",
-            "alldlls=default",
-            "quartz",
-            "dxvk",
-        ])
-        .envs(Options::vars(&options))
-        .output()
-        .unwrap();
-
-    Command::new("winetricks")
-        .arg("-q")
-        .arg("wmp10")
-        .envs(Options::vars(&options))
-        .output()
-        .unwrap();
-
-    Command::new("winetricks")
-        .arg("renderer=gdi")
-        .envs(Options::vars(&options))
-        .output()
-        .unwrap();
+    winetricks(&commands, envs);
 
     Ok("Created successfully.".to_string())
+}
+
+fn winetricks(commands: &[&str], envs: HashMap<&str, String>) {
+    let len = commands.len();
+    let mut i = 1;
+
+    for command in commands {
+        notify(&format!("Running winetricks {command}... ({i}/{len})"), None);
+
+        Command::new("winetricks")
+            .arg("-q")
+            .arg(command)
+            .envs(&envs)
+            .output()
+            .unwrap();
+
+        i += 1;
+    }
 }
