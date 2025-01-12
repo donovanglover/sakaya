80eca9990f707b29cd47718828f11ade2743ed63
Donovan Glover <donovan@dglover.co>
2025-01-12 09:00:24 -0500

    fix: bind server to all addresses
    Should fix an issue where the local_ip was occasionally unreachable.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use clap::Parser;
-use local_ip_address::local_ip;
 use sakaya::cli::Cli;
 use sakaya::cli::Commands;
 use sakaya::util::is_container;
 use sakaya::util::notify;
 use sakaya::{client, server};
+use std::net::Ipv4Addr;
 use std::net::SocketAddrV4;
-use std::net::{IpAddr, Ipv4Addr};
 
 /// The main function is in charge of either starting a `sakaya-server` or
 /// starting a `sakaya-client` that connects to a `sakaya-server`.
@@ -17,23 +16,22 @@ use std::net::{IpAddr, Ipv4Addr};
 async fn main() {
     #[rustfmt::skip]
     let Cli { address, command, file, directory, .. } = Cli::parse();
+    let ip = Ipv4Addr::new(0, 0, 0, 0);
 
-    if let Ok(IpAddr::V4(ip)) = local_ip() {
-        match &command {
-            Some(Commands::Server { port }) => start_server(ip, *port).await,
+    match &command {
+        Some(Commands::Server { port }) => start_server(ip, *port).await,
 
-            None => {
-                if is_container() {
-                    start_server(ip, 39493).await
-                } else if let Some(file) = file {
-                    if let Some(directory) = directory.to_str() {
-                        client::exec(address, &file, directory);
-                    } else {
-                        notify("Invalid directory was given.", None)
-                    }
+        None => {
+            if is_container() {
+                start_server(ip, 39493).await
+            } else if let Some(file) = file {
+                if let Some(directory) = directory.to_str() {
+                    client::exec(address, &file, directory);
                 } else {
-                    notify("sakaya was called but no file was given.", None);
+                    notify("Invalid directory was given.", None)
                 }
+            } else {
+                notify("sakaya was called but no file was given.", None);
             }
         }
     }
