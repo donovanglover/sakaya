58b03869322b70e15421e674be1037449bb6933e
Donovan Glover <donovan@dglover.co>
2025-04-07 00:36:00 -0400

    server: disable automatic winetricks for now
    Disabled while I test what works with wine 10.4.

--- src/server/routes/init.rs
+++ src/server/routes/init.rs
@@ -1,5 +1,5 @@
 use axum::extract::Json;
-use std::{collections::HashMap, path::Path, process::Command};
+use std::{path::Path, process::Command};
 
 use crate::{state::Options, util::notify};
 
@@ -15,47 +15,5 @@ pub async fn init(Json(options): Json<Options>) -> Result<String, &'static str> 
 
     Command::new("wineboot").envs(&envs).output().unwrap();
 
-    let commands = [
-        "fontsmooth=rgb",
-        "dotnet35",
-        "dotnet40",
-        "vcrun2003",
-        "vcrun2005",
-        "vcrun2008",
-        "vcrun2010",
-        "vcrun2012",
-        "vcrun2013",
-        "vcrun2015",
-        "lavfilters",
-        "alldlls=default",
-        "quartz",
-        "dxvk",
-        "wmp10",
-        "renderer=gdi",
-    ];
-
-    winetricks(&commands, envs);
-
     Ok("Created successfully.\n".to_string())
-}
-
-fn winetricks(commands: &[&str], envs: HashMap<&str, String>) {
-    let len = commands.len();
-    let mut i = 1;
-
-    for command in commands {
-        notify(
-            &format!("Running winetricks {command}... ({i}/{len})"),
-            None,
-        );
-
-        Command::new("winetricks")
-            .arg("-q")
-            .arg(command)
-            .envs(&envs)
-            .output()
-            .unwrap();
-
-        i += 1;
-    }
 }
