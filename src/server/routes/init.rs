a9e211e29704ac93cfc857336a8f2d0498912a0a
Donovan Glover <donovan@dglover.co>
2025-04-30 18:44:06 -0400

    init: remove alldlls=default
    Not needed since dlls get used by default.
    
    See https://github.com/Winetricks/winetricks/issues/2088

--- src/server/routes/init.rs
+++ src/server/routes/init.rs
@@ -21,7 +21,6 @@ pub async fn init(Json(options): Json<Options>) -> Result<String, &'static str> 
         "mfc42",
         "vcrun2022",
         "lavfilters",
-        "alldlls=default",
         "quartz",
         "dxvk",
         "wmp10",
